27b37fc7567414eca1f16f594644fda4b01eadbb
Drew Zinck <dlzincktheii@gmail.com>
2023-01-23 01:55:57 -0700

    Finished representation of Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,6 +10,9 @@ const Home = () => {
     if(data?.length > 0) {
       return data.map((post) => <Card key={post._id} {...post} />)
     }
+    return (
+      <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
+    )
   }
 
   return (
@@ -35,7 +38,17 @@ const Home = () => {
             </h2>
            )}
            <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
-
+            {searchText ? (
+              <RenderCards 
+                data={[]}
+                title='No search results found'
+              />
+            ) : (
+              <RenderCards
+                data={[]}
+                title='No posts found'
+              />
+            )}
            </div>
          </>
         )}
