3b1015311d3deaead8da7171e50c3ec14d76cef6
Drew Zinck <dlzincktheii@gmail.com>
2023-02-03 21:17:03 -0700

    Created FormField boiler & empty functions placement

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,12 +15,49 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = () => {
+
+  }
+
+  const handleChnage = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+    
+  }
+
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
         <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
         <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Create imaginative and incredible stunning images through DALL-E AI and share them with the community!</p>
       </div>
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField 
+            LabelName="Your name"
+            type='text'
+            name='name'
+            placeholder='John Doe'
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField 
+            LabelName="Prompt"
+            type='text'
+            name='prompt'
+            placeholder='A man wanders through the rainy streets of Tokyo, with bright neon signs, 50mm'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+
+      </form>
+
     </section>
   )
 }
