2b288b72151660151a10890af401866f6f025cb3
Drew Zinck <dlzincktheii@gmail.com>
2023-02-03 21:08:54 -0700

    Created h1 and p tag to differentiate from Home and Create pages.

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,7 +16,12 @@ const CreatePost = () => {
   const [loading, setLoading] = useState(false);
 
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Create imaginative and incredible stunning images through DALL-E AI and share them with the community!</p>
+      </div>
+    </section>
   )
 }
 
