14b592f6dd8abb5c517360dd1a67be10178c9d75
Drew Zinck <dlzincktheii@gmail.com>
2023-03-23 18:57:34 -0700

    Created the generate image, tested it and it works on first try! Need to play lotto now... brb

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,9 +15,31 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {
-
-  }
+  const generateImage = async () => {
+    if(form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8000/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
+        
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide a proper prompt');
+    }
+  };
 
   const handleSubmit = () => {
 
@@ -28,7 +50,7 @@ const CreatePost = () => {
   }
 
   const handleSurpriseMe = () => {
-    const randomPrompt = getRandomPrompt(fomr.prompt);
+    const randomPrompt = getRandomPrompt(form.prompt);
     setForm({ ...form, prompt: randomPrompt })
   }
 
